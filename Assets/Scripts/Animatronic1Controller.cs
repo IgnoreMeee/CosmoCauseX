f7ee87a103f545a63f66f03bb5a90084335a1566
ryantcoffee <iamryant2@gmail.com>
2026-05-21 21:42:50 -0400

    made movement system to fit new map

--- Assets/Scripts/Animatronic1Controller.cs
+++ Assets/Scripts/Animatronic1Controller.cs
@@ -51,7 +51,7 @@ public class FreddyController : MonoBehaviour
             if (UnityEngine.Random.Range(1, 2) == 1)
             {
                 Move();
-                Debug.Log("Freddy: " + locationIndex);
+                Debug.Log("Index: " + locationIndex);
             }
            
             
@@ -67,13 +67,26 @@ public class FreddyController : MonoBehaviour
     {
         switch (locationIndex)
         {
+
+            case 1:
+                locationIndex = getRandom(new int[] {10, 13});
+                break;
+            case 2:
+                locationIndex = getRandom(new int[] {3, 8});
+                break;
+            case 3:
+                locationIndex = getRandom(new int[] {4, 5});
+                break;
             case 4:
                 locationIndex = 6;
                 break;
             case 5:
-                locationIndex = 7;
+                locationIndex = getRandom(new int[] {6, 9});
                 break;
             case 6:
+                locationIndex = 7;
+                break;
+            case 7: //jumpscare case
                 if (doorControl.leftDoorClosed == false)
                 {
                     jumpscare();
@@ -83,8 +96,35 @@ public class FreddyController : MonoBehaviour
                     locationIndex = 0;
                 }
                 break;
-
-            case 7:
+            case 8:
+                locationIndex = 9;
+                break;
+            case 9:
+                if (doorControl.leftDoorClosed == false)
+                {
+                    jumpscare();
+                    locationIndex = 0;
+                } else
+                {
+                    int continueAttack = getRandom(new int[] {1, 2});
+                    if (continueAttack == 1) //continue
+                    {
+                        locationIndex = 5;
+                    }
+                    else //retreat
+                    {
+                        locationIndex = 0;
+                    }
+                    
+                }
+                break;
+            case 10:
+                locationIndex = getRandom(new int[] {11, 12});
+                break;
+            case 11:
+                locationIndex = 10;
+                break;
+            case 12:
                 if (doorControl.rightDoorClosed == false)
                 {
                     jumpscare();
@@ -93,13 +133,32 @@ public class FreddyController : MonoBehaviour
                 {
                     locationIndex = 0;
                 }
+            break;
+            case 13:
+                locationIndex = 14;
                 break;
-
+            case 14:
+                if (doorControl.rightDoorClosed == false)
+                {
+                    jumpscare();
+                    locationIndex = 0;
+                } else
+                {
+                    locationIndex = 0;
+                }
+            break;
+    
             default:
                 locationIndex += UnityEngine.Random.Range(1, 3);
-                break;
-        
+                break;        
         }
+    }
+
+    public int getRandom(int[] array)
+    {
+        int randomIndex = UnityEngine.Random.Range(0, array.Length);
+        int result = array[randomIndex];
+        return result;
     }
 
     void jumpscare()
