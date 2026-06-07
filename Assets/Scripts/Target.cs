c7664d07626e162c774384b08271984e22a25d22
Rodean Saleh <saleh.rodean@gmail.com>
2026-06-06 23:01:52 -0400

    this also connetcs with asteroid.cs now yay

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
-
 public class Target : MonoBehaviour
 {
     float health = 30f;
-
+    Astroid manager;
+    void Start()
+    {
+        manager = FindFirstObjectByType<Astroid>();
+    }
     public void TakeDamage(float amt)
     {
         health -= amt;
         if (health <= 0f)
         {
-            Destroy(gameObject);
+            if (manager != null)
+            {
+                manager.OnAsteroidDestroyed(gameObject);
+            }   
             SoundManager.Instance.PlaySFX(SoundManager.Instance.astroidExplode);
+            Destroy(gameObject);
             Debug.Log("i died");
         }
     }
